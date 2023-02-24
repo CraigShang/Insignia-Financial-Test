ef21d79389791f1a10a96c3fa5dc55edeb9d6e3a
Craig <shangpeizheng@gmail.com>
2023-02-24 11:04:24 +1100

    Create CommandInterpreter.java

---
+++ src/CommandInterpreter.java
@@ -0,0 +1,40 @@
+public class CommandInterpreter {
+    private Robot robot;
+
+    public CommandInterpreter(Robot robot) {
+        this.robot = robot;
+    }
+
+    public void interpret(String commandString) {
+        String[] tokens = commandString.trim().split("\\s+");
+        if (tokens.length == 0) {
+            return; // ignore empty lines
+        }
+        Command command = Command.valueOf(tokens[0].toUpperCase());
+        switch (command) {
+            case PLACE:
+                if (tokens.length == 4) {
+                    int x = Integer.parseInt(tokens[1]);
+                    int y = Integer.parseInt(tokens[2]);
+                    Direction direction = Direction.valueOf(tokens[3].toUpperCase());
+                    robot.place(x, y, direction);
+                }
+                break;
+            case MOVE:
+                robot.move();
+                break;
+            case LEFT:
+                robot.rotateLeft();
+                break;
+            case RIGHT:
+                robot.rotateRight();
+                break;
+            case REPORT:
+                robot.report();
+                break;
+            default:
+                System.err.println("Unknown command: " + commandString);
+                break;
+        }
+    }
+}
