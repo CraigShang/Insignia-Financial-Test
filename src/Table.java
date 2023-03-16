48c512e7b0bfaae31c18d11d8b110784e59c6182
Craig <shangpeizheng@gmail.com>
2023-03-16 11:20:30 +1100

    Update Table.java

--- src/Table.java
+++ src/Table.java
@@ -5,19 +5,4 @@ public class Table {
     public boolean isValidPosition(int x, int y) {
         return x >= 0 && x < width && y >= 0 && y < height;
     }
-
-    public boolean willFall(int x, int y, Direction direction) {
-        switch (direction) {
-            case NORTH:
-                return y == height - 1;
-            case SOUTH:
-                return y == 0;
-            case EAST:
-                return x == width - 1;
-            case WEST:
-                return x == 0;
-            default:
-                return true;
-        }
-    }
 }
